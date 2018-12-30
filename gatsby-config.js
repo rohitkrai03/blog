3f32be64bf87120778ef5993343fe2f028cca21d
Rohit Rai <rohitkrai03@gmail.com>
2018-12-31 00:54:39 +0530

    Updated profile pic and details

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    title: `Arkpod Blog`,
+    author: `Rohit Rai`,
+    description: `I write code and I know things.`,
+    siteUrl: `http://arkpod.in/blog/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `rohitkrai03`,
     },
   },
   plugins: [
@@ -57,8 +57,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `Arkpod Blog`,
+        short_name: `Arkblog`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
--- src/components/Bio.js
+++ src/components/Bio.js
@@ -28,12 +28,9 @@ function Bio() {
               }}
             />
             <p>
-              Written by <strong>{author}</strong> who lives and works in San
-              Francisco building useful things.
+              Personal blog by <strong>{author}</strong>
               {` `}
-              <a href={`https://twitter.com/${social.twitter}`}>
-                You should follow him on Twitter
-              </a>
+              I write code and I know things.
             </p>
           </div>
         )
